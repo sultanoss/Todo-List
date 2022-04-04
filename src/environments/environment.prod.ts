6595e4aeb2da259317531ba4ff435278be66f084
sultanoss <radwansultan@hotmail.dem>
2022-04-04 14:59:19 +0200

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'todo-list-aa856',
+    appId: '1:809385998777:web:88823abefb8e1def3b1e68',
+    storageBucket: 'todo-list-aa856.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyD6f30cVpe8Ubh0L0KAhJ-QDTaDMz6MQQA',
+    authDomain: 'todo-list-aa856.firebaseapp.com',
+    messagingSenderId: '809385998777',
+  },
+  production: true
+};
